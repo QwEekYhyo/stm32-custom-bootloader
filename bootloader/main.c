937770faaa8eb45ad7f246a4028cbf1e0542334f
Logan Lucas <logan.lucas83@gmail.com>
2026-03-06 18:08:35 +0100

    bootloader turns on LED just to prove its presence

--- bootloader/main.c
+++ bootloader/main.c
@@ -5,6 +5,19 @@
 typedef void (*entry_t)(void);
 
 int main(void) {
+    // Enable RCC for GPIOB
+    *((volatile uint32_t*) 0x4002104C) |= 1 << 1;
+
+    // Enable RCC for SYSCFG
+    *((volatile uint32_t*) 0x40021060) |= 1 << 0;
+
+    // Set PB8 as output (0b01)
+    *((volatile uint32_t*) 0x48000400) |= 1 << 16;
+    *((volatile uint32_t*) 0x48000400) &= ~(1 << 17);
+
+    // Turn on LED
+    *((volatile uint32_t*) 0x48000414) |= 1 << 8;
+
     // Get app stack pointer
     uint32_t stack = *(volatile uint32_t*)APP_ADDRESS;
 
